6dfb81dcdd2dc9c4f294d99d275828b1d83167c0
Valdecir Teodoro <clixsensevt42@gmail.com>
2026-09-08 19:05:47 -0300

    Ajusta SEO e remove garantia

---
+++ src/app/robots.ts
@@ -0,0 +1,13 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+    sitemap: "https://lift-gold-premium.vercel.app/sitemap.xml",
+  };
+}
